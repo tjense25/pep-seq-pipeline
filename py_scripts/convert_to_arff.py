1bddde7c7534e00b91f592b9c73ab40071344fe8
tjense25 <tanner.jensen08@gmail.com>
2018-02-02 16:37:18 -0700

    first steps of pipeline working :D

--- py_scripts/convert_to_arff.py
+++ py_scripts/convert_to_arff.py
@@ -1,32 +1,33 @@
 #! usr/bin/env python
 import sys
 
-residues = 'RHKDESTNQCGPAVILFMYW'
-if len(sys.argv) == 2 and sys.argv[1].endswith('.csv'):
-	InFile = open(sys.argv[1], 'rU')
-	outFileName = sys.argv[1].strip('.csv') + '.arff'
-	OutFile = open(outFileName, 'w')
-	OutFile.write('@relation pep-seq\n')
-	res_nominal = '{R,H,D,S,N,C,G,V,I,L,F,Y}'
-	for i in range(1, 9):
-		OutFile.write('@attribute pos%d %s\n' % (i, res_nominal))
-	OutFile.write('@attribute toxicity  {toxic, neutral, anti-toxic}\n')
-	OutFile.write('@data\n')
-	linenumber = 0
-	for line in InFile:
-		if(linenumber == 0):
-			linenumber += 1
-			continue
-		else:
-			columns = line.strip('\n').split(',')
-			pepseq = columns[0];
-			toxicity = columns[8];
-			for r in pepseq:
-				if r not in residues:
-					r = "?"
-				OutFile.write(r + ',')
-			OutFile.write('%s\n' % toxicity)
-	InFile.close();
-	OutFile.close();
-else:
-	print("ERROR")
+def main():
+	if len(sys.argv) == 2:
+		InFile = open(sys.argv[1], 'rU')
+		sys.stdout.write('@relation pep-seq\n')
+		residues = 'RHKDESTNQCGPAVILFMYW'
+		res_nominal = '{R,H,D,S,N,C,G,V,I,L,F,Y}'
+		for i in range(1, 9):
+			sys.stdout.write('@attribute pos%d %s\n' % (i, res_nominal))
+		sys.stdout.write('@attribute toxicity  {toxic, neutral, anti-toxic}\n')
+		sys.stdout.write('@data\n')
+		linenumber = 0
+		for line in InFile:
+			if(linenumber == 0):
+				linenumber += 1
+				continue
+			else:
+				columns = line.strip('\n').split(',')
+				pepseq = columns[0];
+				toxicity = columns[8];
+				for r in pepseq:
+					if r not in residues:
+						r = "?"
+					sys.stdout.write(r + ',')
+				sys.stdout.write('%s\n' % toxicity)
+		InFile.close();
+	else:
+		print("ERROR: INPUT DATA FILE NOT GIVEN")
+
+if __name__ == "__main__":
+	main()
--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#Check to see if there is at least one command line argument
+if [ $# -lt 1 ]
+then
+	echo "ERROR: You must specify input file as command line argument"
+	echo "$USAGE"
+	exit 1
+fi
+
 INPUT_FILE=$1
 shift
 
@@ -11,6 +19,9 @@ then
 	exit 1
 fi
 
+#Initialize parameter values
+K=30
+
 #Iterate through command line arguments and store data into variables
 while [ $# -gt 0 ]
 do
@@ -18,11 +29,6 @@ do
 	case "$ARG" in
 		--arff) #data already in arff format
 			arff=true
-			;;
-		-T)  #Specifies test file to run in the machine learning algorithm
-			test_file=true
-			TEST=$2
-			shift
 			;;
 		-k) #Specify number of motifs to test (default is 30)
 			K=$2
@@ -37,6 +43,11 @@ do
 		-b) #Balance the data before running random forest classifier
 			balance=true
 			;;
+		-o) #Save output of the model to a new direcrtory in results
+			output=true
+			OUTDIR=$2
+			shift
+			;;
 		*) #Default branch, print argument not valid and usage then exit
 			echo "Not a valid argument: $1"
 			echo "$USAGE"
@@ -47,17 +58,42 @@ do
 done
 
 #IF balance parameter was passed in, run python balance script on the input data
-if [ balance && ! arff ]
+if [ $balance ] && [ ! $arff ]
 then
-	python py_scripts/balance_data.py $INPUT_FILE &> temp.txt
-	INPUT_FILE=temp.txt
+	>&2 echo "Balancing Input Data . . . "
+	python py_scripts/balance_data.py $INPUT_FILE &> balancedtemp.csv
+	INPUT_FILE=balancedtemp.csv
 fi
 
 #If passed in input file is not already an arff file, convert it to arff format
-if [ ! arff ]
+if [ ! $arff ]
 then
-	python py_scripts/convert_to_arff.py $INPUT_FILE &> temp.txt
-	INPUT_FILE=temp.txt
+	>&2 echo "Converting Input Data to Arff Format . . ."
+	python py_scripts/convert_to_arff.py $INPUT_FILE &> arfftemp.arff
+	INPUT_FILE=arfftemp.arff
+	rm -f balancedtemp.csv
+fi
+
+#Run Weka's random forest classifiers on the arff file and store the tree output into temp.txt
+>&2 echo "Running Random Forest Classification Algorithm on Data . . . "
+java -cp dependency_jars/weka.jar weka.classifiers.trees.RandomForest -U -B -P 50 -I 500 -no-cv -print -t $INPUT_FILE&> foresttemp.txt
+rm -f arfftemp.arff
+
+#Take the output of weka's Random Forest classifier and put it into our MotifFounder Algorithm
+>&2 echo "Finding Motifs From Random Forest . . . "
+java -jar dependency_jars/MotifFinder.jar foresttemp.txt $K &> motifs.txt
+rm -f foresttemp.txt
+
+#IF output is true, save the output file to the specifies directory in results. If it does not exist, create such a directory
+#If output not specified print out the motifs data to standard output
+if [ output ]
+then
+	>&2 echo "Saving Output to results/$OUTDIR . . . "
+	mkdir results/$OUTDIR 2> /dev/null
+	mv motifs.txt results/$OUTDIR
+else
+	cat motifs.txt
+	rm -f motifs.txt
 fi
 
 exit 0
